2299e1ac93535e37caa9550bf773e0c1b14daac5
VeldinS <190302158@student.ius.edu.ba>
2021-12-08 11:23:12 +0100

    project3

--- script.js
+++ script.js
@@ -1,17 +1,100 @@
 //BACK TO TOP BUTTON//
-function goToTop () {
+function goToTop () {           
     window.scrollTo({
         top:0,
         behavior: "smooth"
     });
 }
 
-window.addEventListener("scroll", function name(){
+
+/***for project 3 ***/
+const date = new Date();            //1. Object 
+const hour = date.getHours();       //and accessing object
+
+$(document).ready(function (){        //2. Function
+    
+    if(hour >= 6 && hour <= 12){        //7. if-else statement
+        $(".morning").addClass('goodMorning');
+    }
+    else if(hour >= 13 && hour <= 20){
+        $(".afternoon").addClass('goodAfternoon');
+    }
+    else if(hour >= 21 && hour <= 5){
+        $(".evening").addClass('goodEvening');
+    }
+    })
+
+var morningGreet = document.createElement("span");   //4. Variables
+morningGreet.innerHTML = "<h3>Have a nice day!</h3>";
+
+var afternoonGreet = document.createElement("span");
+afternoonGreet.innerHTML = "<h3>Have a nice rest of the day!</h3>";
+
+var eveningGreet = document.createElement("span");
+eveningGreet.innerHTML = "<h3>Have a nice rest of the day!</h3>";
+
+var x;
+
+if(hour >= 6 && hour <= 12){        
+    x = 1;
+}
+else if(hour >= 13 && hour <= 20){
+    x = 2;
+}
+else if(hour >= 21 && hour <= 5){
+    x = 3;
+}
+
+function insertElement(targetElement, newElement) {
+    targetElement.parentNode.insertBefore(newElement, targetElement.nextSibling);
+}
+
+switch (x){         //8. Switch
+    case 1:
+        insertElement(this.document.getElementById("morning"), morningGreet);
+        break;
+    case 2:
+        insertElement(this.document.getElementById("afternoon"), afternoonGreet);
+        break;
+    case 3:
+        insertElement(this.document.getElementById("evening"), eveningGreet);
+}
+
+//ARRAY EXAMPLE CANNOT BE USED ON PAGE
+//CONTENT PANEL WITH TEXT ONLY BECAUSE PANELS ON MY PAGE ARE ALREADY SET
+const coll = document.getElementsByClassName("contentPanel");       //10. Content Panel
+let i;
+
+for (i = 0; i < coll.length; i++) {                             //8. For Loop
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
+
+//HIDE TOP BUTTON AFTER SCROLL//
+window.addEventListener("scroll", function name(){        //5. Event
     const top_btn = this.document.getElementById("top-btn");
     if (top_btn !== null){
         top_btn.classList.toggle("show-top-btn", window.scrollY > 200)
     }
 })
+
+//BIO SECTION ANIMATION ON SCROLL//
+$(document).ready(function (){
+    $(".biopart").waypoint(function(direction){                         //6. Adding animation
+        $(".animation1").addClass('animate__animated animate__fadeIn');
+    },{
+        offset:'50%'
+    });
+})
+
+
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
